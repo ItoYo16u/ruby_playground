f403feb8c5428fc1a4bc9faeb07aa30e9d5289f1
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-02-18 00:07:56 +0900

    add calc change

---
+++ changes.rb
@@ -0,0 +1,10 @@
+def calcChange(paid_money,item_name)
+	items = {tea: 120, cola: 150, energy_drink: 210}
+	changes = {500=> 0, 100=> 0,50=> 0,10 => 0}
+	money_to_return = paid_money - items[item_name]
+	changes.each do |k,v|
+		changes[k] += money_to_return/k
+		money_to_return %= k
+	end
+	changes
+end
