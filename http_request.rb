6df13ae69b7997cdc328365993d1f680d9f09e06
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-03-06 01:22:45 +0900

    add http and fizzbuzz

---
+++ http_request.rb
@@ -0,0 +1,8 @@
+require "net/http"
+require "json"
+
+q = URI.encode_www_form_component("parameter") 
+uri = URI.encode("https://path/to/endpoint?q=#{q}")
+res = Net::HTTP.get_response(uri)
+res_json = JSON.parse(res.body)
+
