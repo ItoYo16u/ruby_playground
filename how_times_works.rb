8fe8e6be2e1714e459559f9853b8a5984e35cab7
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-04-14 23:37:52 +0900

    add times

---
+++ how_times_works.rb
@@ -0,0 +1,23 @@
+5.times { |n|
+	puts n
+}
+# 0
+# 1
+# 2
+# 3
+# 4
+#  => 5
+-5.times{ |n|
+	puts n
+}
+#=> -5
+
+0.times {|n|
+	puts n
+}
+#=> 0
+
+1.5.times{|n|
+	puts n
+}
+#=> NoMethodError: undefined method times for 1.5 float
