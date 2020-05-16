d2b74cacb9631f3abb9bc4fd29914357a97954ed
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-05-16 18:51:29 +0900

    add recursive split

---
+++ recursive_split.rb
@@ -0,0 +1,11 @@
+def recursive_split(n)
+	s_arr = n.to_s.split("")
+	sum = s_arr.reduce(0){|acc,s| acc += s.to_i}
+	if sum > 10
+		recursive_split(sum)
+	else
+		n+sum
+	end
+end
+
+puts recursive_split(98)
